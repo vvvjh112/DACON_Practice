8704ec31c6870b7eaede5d245b30b5de365ec2ef
vvvjh112 <lleell6@naver.com>
2023-06-21 13:15:50 +0900

    ...모델링 및 예측 (선형)

--- prac.py
+++ prac.py
@@ -3,6 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# train 파일의 데이터를 학습시켜서 모델을 통해 test의 컬럼들의 데이터를 이용하여 예측하여 submission파일에 출력
+
+
+#출력 설정
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 submission = pd.read_csv("submission.csv")
@@ -62,4 +69,77 @@ train = train.fillna(train.mean())
 
 ### test 프레임 ###
 test['hour_bef_precipitation'].fillna(value = '0', inplace=True)
-print(test.isna().sum())
+# print(test.isna().sum())
+
+# 평균
+# print(test['hour_bef_temperature'].mean())
+# print(test['hour_bef_windspeed'].mean())
+# print(test['hour_bef_humidity'].mean())
+# print(test['hour_bef_visibility'].mean())
+# print(test['hour_bef_ozone'].mean())
+# print(test['hour_bef_pm10'].mean())
+# print(test['hour_bef_pm2.5'].mean())
+
+
+
+
+test_isna = test.isna().sum()
+
+
+
+# test 결측치를 평균값으로 대체
+# test = test.fillna(test.mean()) 로 진행할 시 오류나서 na 컬럼만 대체
+def fill_bicycle_na(df, column):
+    df[column] = df[column].fillna(value=df[column].mean())
+
+na_columns = test_isna[test_isna != 0].index
+for col in na_columns:
+    fill_bicycle_na(test,col)
+
+
+#train 데이터에서 시간과 대여수 그래프
+# plt.plot('hour', 'count', 'o', data=train)
+# plt.show()
+
+#히트맵
+# plt.figure(figsize = (12,12))
+# sns.heatmap(train.corr(), annot = True)
+# plt.show()
+
+#컬럼
+xfet = ['hour', 'hour_bef_temperature', 'hour_bef_windspeed', 'hour_bef_humidity', 'hour_bef_visibility','hour_bef_ozone', 'hour_bef_pm10', 'hour_bef_pm2.5']
+
+xfets = train[xfet].columns
+
+# 각 컬럼 컬럼들과 대여량 과의 관계 시각화
+# for i in xfets:
+#     sns.lmplot(x=i, y='count', data=train)
+#     plt.show()
+    
+
+############################################################################
+
+# 모델 import
+from sklearn.linear_model import LinearRegression               #선형회귀
+from sklearn.ensemble import RandomForestRegressor              #랜덤포레스트
+from sklearn.model_selection import GridSearchCV                #GVsearch
+from sklearn.model_selection import RandomizedSearchCV          #RandomizedSearch
+from sklearn.ensemble import GradientBoostingRegressor          #그래디언트 부스팅
+
+features = ['hour', 'hour_bef_temperature', 'hour_bef_ozone', 'hour_bef_windspeed', 'hour_bef_humidity']
+
+# X독립변수  Y 종속변수
+X_train = train[features]
+y_train = train['count']
+X_test = test[features]
+
+#선형회귀모델 선택
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+#예측
+lr_predict = lr.predict(X_test)
+
+#submission 파일에 예측값 대입 및 내보내기
+submission['count'] = lr_predict
+submission.to_csv('lr.csv', index=False)
