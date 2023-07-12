d60564941d47999a202a8e206296323c30bc8fc1
vvvjh112 <lleell6@naver.com>
2023-07-12 18:05:26 +0900

    .

--- Energy/First.py
+++ Energy/First.py
@@ -1,7 +1,8 @@
 ## 전처리는 baseline을 바탕으로 랜덤포레스트 및 gridSearchCV 사용해서 점수 올려볼 계획
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -101,6 +102,35 @@ for i, q in enumerate(np.arange(0.1, 1, 0.1)):
     y_pred = np.percentile(rf_preds, q * 100, axis=0)
     submission.iloc[:, i] = np.ravel(y_pred)
 
-submission.to_csv(f'rf_gs.csv')
+# submission.to_csv(f'rf_gs.csv')
 
 #RandomizedSearchCV 를 이용해서 더 튜닝 해보기
+# rs_model = RandomForestRegressor()
+# param = {'min_samples_split': range(12,15),
+#         'max_depth': range(8,11),
+#         'n_estimators': range(222,225)}
+# print("시작")
+# rs = RandomizedSearchCV(estimator=rs_model, param_distributions=param, scoring = 'neg_mean_squared_error', cv=3)
+#
+# rs.fit(train_data, train_label)
+#
+# print('최고 정확도 : ', rs.best_score_)
+# print('최고 파라미터 : ', rs.best_params_)
+
+# rf = ensemble.RandomForestRegressor(n_estimators=224,
+#                                     max_features=1, random_state=0,
+#                                     max_depth = 8,
+#                                     verbose=True,
+#                                     n_jobs=-1, min_samples_split=13)
+# rf.fit(train_data, train_label)
+#
+# rf_preds = []
+# for estimator in rf.estimators_:
+#     rf_preds.append(estimator.predict(test))
+# rf_preds = np.array(rf_preds)
+#
+# for i, q in enumerate(np.arange(0.1, 1, 0.1)):
+#     y_pred = np.percentile(rf_preds, q * 100, axis=0)
+#     submission.iloc[:, i] = np.ravel(y_pred)
+#
+# submission.to_csv(f'rf_rs.csv')
