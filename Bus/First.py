97f8299b5dd8050151ef5ee1a6e17e1863e68111
vvvjh112 <lleell6@naver.com>
2023-08-02 17:14:55 +0900

    .

--- Bus/First.py
+++ Bus/First.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
 
 #데이터를 바탕으로 도착시간을 예측해야 함
 pd.set_option('display.max_columns', None) ## 모든 열을 출력한다.
@@ -23,6 +24,8 @@ pd.set_option('display.max_rows', None)
 
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
+train_data = pd.read_csv('train.csv')
+test_data = pd.read_csv('test.csv')
 
 # print(train.columns)
 def df_del(df):
@@ -54,39 +57,99 @@ def dist_check(df):
     for i in range(df.shape[0]):
         temp['hot'][i] = tmp(temp,i)
 
-        tmmp = temp['now_arrive_time'][i]
-        tmmp = tmmp[:2]
-        temp['now_arrive_time'][i] = int(tmmp)
+        # tmmp = temp['now_arrive_time'][i]
+        # tmmp = tmmp[:2]
+        # temp['now_arrive_time'][i] = int(tmmp)
 
     return temp
 
-tmp = df_del(train)
-tmp = dist_check(tmp)
-tmp = tmp.drop(['now_latitude','now_longitude','next_latitude','next_longitude'],axis=1)
-print(tmp.columns)
+#
+# tmp = df_del(train)
+# tmp = dist_check(tmp)
+# tmp = tmp.drop(['now_latitude','now_longitude','next_latitude','next_longitude'],axis=1)
+# print(tmp.columns)
+#
+#
+# X_train = tmp.drop(['id', 'next_arrive_time'],axis=1)
+#
+# #데이터타입 변경
+# X_train.set_index('now_arrive_time',inplace=True)
+# X_train = X_train.astype('int64')
+# X_train.reset_index(inplace=True)
 
-
-X_train = tmp.drop(['id', 'next_arrive_time'],axis=1)
-#데이터타입 변경
-X_train.set_index('now_arrive_time',inplace=True)
-X_train = X_train.astype('int64')
-X_train.reset_index(inplace=True)
-
-
-print("데이터 타입" , X_train.dtypes)
-y_train = tmp['next_arrive_time']
+# y_train = tmp['next_arrive_time']
 #Index(['id', 'now_arrive_time', 'distance', 'next_arrive_time', 'hot'], dtype='object')
-#테스트
-print(tmp.head())
-# print("\n")
-# print(X_train.head())
+
+
 
 ##### 내가한 전처리는 효과가 별로인 것 같음..
 #2차시도
 #핫플은 유지하고  음..
+station_encoder = LabelEncoder() # 인코더 생성
+
+_station = list(train_data['now_station'].values) + list(train_data['next_station'].values) # train_data 의 모든 정류장 이름
+station_set = set(_station)
+# print(len(station_set))
+station_encoder.fit(list(station_set)) # 인코딩
+
+# 모든 학습, 시험 데이터의 정류장 정보 치환
+train_data['now_station'] = station_encoder.transform(train_data['now_station'])
+train_data['next_station'] = station_encoder.transform(train_data['next_station'])
+test_data['now_station'] = station_encoder.transform(test_data['now_station'])
+test_data['next_station'] = station_encoder.transform(test_data['next_station'])
+# print(train_data.head())
+
+train_data['next_arrive_time'].hist()
+# plt.show()
+
+## train data
+
+train_data['date'] = pd.to_datetime(train_data['date']) # date 값을 datetime으로
+train_data['weekday'] = train_data['date'].dt.weekday  # Monday 0, Sunday 6
+train_data['weekday'] = train_data['weekday'].apply(lambda x: int(0) if x <= 5 else int(1))
+# 0 ~ 5 는 월요일 ~ 금요일이므로 평일이면 0, 주말이면 1을 설정하였다
+
+train_data = pd.get_dummies(train_data, columns=['weekday']) # 평일/주말에 대해 One-hot Encoding
+
+train_data = train_data.drop('date', axis=1) # 필요없는 date 칼럼을 drop
+# print(train_data.head())
+
+train_data['time_group']='group' #time_group 변수를 미리 생성
+
+train_data.loc[ (train_data['now_arrive_time']>='05시') & (train_data['now_arrive_time']<'12시') ,['time_group'] ]= 'morning' # 05~11시
+train_data.loc[ (train_data['now_arrive_time']>='12시') & (train_data['now_arrive_time']<'18시') ,['time_group'] ]= 'afternoon' #12~17시
+train_data.loc[ (train_data['now_arrive_time']>='18시') | (train_data['now_arrive_time']=='00시'),['time_group'] ]= 'evening' #18~00시
+
+train_data = pd.get_dummies(train_data,columns=['time_group']) # 원 핫 인코딩을 수행
+train_data = train_data.drop('now_arrive_time', axis=1) # 필요없는 now_arrive_time drop
+
+test_data['time_group']='group' #time_group 변수를 미리 생성
+
+test_data.loc[ (test_data['now_arrive_time']>='05시') & (test_data['now_arrive_time']<'12시') ,['time_group'] ]= 'morning' # 05~11시
+test_data.loc[ (test_data['now_arrive_time']>='12시') & (test_data['now_arrive_time']<'18시') ,['time_group'] ]= 'afternoon' #12~17시
+test_data.loc[ (test_data['now_arrive_time']>='18시') | (test_data['now_arrive_time']=='00시'),['time_group'] ]= 'evening' #18~00시
+
+test_data = pd.get_dummies(test_data,columns=['time_group']) # 원 핫 인코딩을 수행
+test_data = test_data.drop('now_arrive_time', axis=1) # 필요없는 now_arrive_time drop
 
 
 
+train_data= dist_check(train_data)
+test_data = dist_check(test_data)
+
+train_data = pd.get_dummies(train_data, columns=['hot']) # 핫플 통과 여부에 대해 One-hot Encoding
+# train_data = train_data.drop('hot', axis=1) # 필요없는 칼럼을 drop
+
+test_data = pd.get_dummies(test_data, columns=['hot']) # 핫플 통과 여부에 대해 One-hot Encoding
+# test_data = test_data.drop('hot', axis=1) # 필요없는 칼럼을 drop
+
+train_data = train_data.drop(['id', 'route_nm', 'next_latitude', 'next_longitude',
+                              'now_latitude', 'now_longitude'], axis=1)
+test_data = test_data.drop(['route_nm', 'next_latitude', 'next_longitude',
+                              'now_latitude', 'now_longitude'], axis=1)
+
+print(train_data.head())
+print(test_data.head())
 
 #이상값 체크 및 모델 스코어 비교 해보자
 
@@ -129,28 +192,28 @@ model_dict = {'DT':DecisionTreeRegressor(),
 
 from sklearn.model_selection import GridSearchCV
 
-
-def get_best_params(model, param):
-    grid_model = GridSearchCV(model, param_grid=param, scoring='neg_mean_squared_error', cv=5, n_jobs=-1)
-    grid_model.fit(X_train, y_train)
-    rmse = np.sqrt(-1* grid_model.best_score_)
-    print('최적 평균 RMSE 값:', np.round(rmse, 4))
-    print('최적 파라미터:', grid_model.best_params_)
-    return grid_model.best_estimator_
-
-xg_param = {
-        #  'max_depth':range(2,10,2),
-        #
-        # 'n_estimators': range(400,1050,100)
-        'n_estimators' : [100,200,300,400,500],
-        # 'learning_rate' : [0.01,0.05,0.1,0.15],
-        # 'max_depth' : [3,5,7,10,15],
-        # 'gamma' : [0,1,2,3],
-        # 'colsample_bytree' : [0.8,0.9],
-}
-
-xgb = xgb.XGBRegressor(colsample_bytree= 0.8, gamma= 0, learning_rate= 0.15, max_depth= 10)
-
-best_xgb = get_best_params(xgb,xg_param)
-
-print(best_xgb)
+#
+# def get_best_params(model, param):
+#     grid_model = GridSearchCV(model, param_grid=param, scoring='neg_mean_squared_error', cv=5, n_jobs=-1)
+#     grid_model.fit(X_train, y_train)
+#     rmse = np.sqrt(-1* grid_model.best_score_)
+#     print('최적 평균 RMSE 값:', np.round(rmse, 4))
+#     print('최적 파라미터:', grid_model.best_params_)
+#     return grid_model.best_estimator_
+#
+# xg_param = {
+#         #  'max_depth':range(2,10,2),
+#         #
+#         # 'n_estimators': range(400,1050,100)
+#         'n_estimators' : [100,200,300,400,500],
+#         # 'learning_rate' : [0.01,0.05,0.1,0.15],
+#         # 'max_depth' : [3,5,7,10,15],
+#         # 'gamma' : [0,1,2,3],
+#         # 'colsample_bytree' : [0.8,0.9],
+# }
+#
+# xgb = xgb.XGBRegressor(colsample_bytree= 0.8, gamma= 0, learning_rate= 0.15, max_depth= 10)
+#
+# best_xgb = get_best_params(xgb,xg_param)
+#
+# print(best_xgb)
