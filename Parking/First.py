ac3aabd6d8ba65b999c6a744dcbe16429d815d08
lleell6 <lleell6@naver.com>
2023-12-19 15:49:09 +0900

    Changes

--- Parking/First.py
+++ Parking/First.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+#plt 한글출력
+plt.rcParams['font.family'] ='Malgun Gothic'
+plt.rcParams['axes.unicode_minus'] =False
+
 # row 생략 없이 출력
 pd.set_option('display.max_rows', None)
 # col 생략 없이 출력
@@ -12,12 +16,13 @@ test = pd.read_csv('test.csv')
 gender = pd.read_csv('age_gender_info.csv')
 submission = pd.read_csv('sample_submission.csv')
 
+train.rename(columns={'도보 10분거리 내 지하철역 수(환승노선 수 반영)': '지하철', '도보 10분거리 내 버스정류장 수': '버스'},inplace=True)
+test.rename(columns={'도보 10분거리 내 지하철역 수(환승노선 수 반영)': '지하철', '도보 10분거리 내 버스정류장 수': '버스'},inplace=True)
+
 # ['단지코드', '총세대수', '임대건물구분', '지역', '공급유형', '전용면적', '전용면적별세대수', '공가수',
-#        '자격유형', '임대보증금', '임대료', '도보 10분거리 내 지하철역 수(환승노선 수 반영)',
-#        '도보 10분거리 내 버스정류장 수', '단지내주차면수', '등록차량수']
+#        '자격유형', '임대보증금', '임대료', '지하철', '버스', '단지내주차면수', '등록차량수']
 # ['단지코드', '총세대수', '임대건물구분', '지역', '공급유형', '전용면적', '전용면적별세대수', '공가수',
-#        '자격유형', '임대보증금', '임대료', '도보 10분거리 내 지하철역 수(환승노선 수 반영)',
-#        '도보 10분거리 내 버스정류장 수', '단지내주차면수']
+#        '자격유형', '임대보증금', '임대료', '지하철', '버스, '단지내주차면수']
 # ['지역', '10대미만(여자)', '10대미만(남자)', '10대(여자)', '10대(남자)', '20대(여자)',
 #        '20대(남자)', '30대(여자)', '30대(남자)', '40대(여자)', '40대(남자)', '50대(여자)',
 #        '50대(남자)', '60대(여자)', '60대(남자)', '70대(여자)', '70대(남자)', '80대(여자)',
@@ -27,19 +32,72 @@ submission = pd.read_csv('sample_submission.csv')
 
 # print(train.info())
 # print(train.head())
-
-#임대료에 - 를 0으로 바꿔줘야함
-#자격유형 BFO가 test에는 없음 - 삭제해도 될 듯
-a = train['자격유형'].value_counts().reset_index(name = 'name').sort_values('자격유형',ascending=True)
-b = test['자격유형'].value_counts().reset_index(name = 'name').sort_values('자격유형',ascending=True)
-print(a)
-print(b)
-#지하철/정류장 수도 2개를 넘어가면 2, 1개면 1, 없으면 0 3가지로 해도 괜찮을 듯
+#
+# a = train['지역'].unique()
+# b = test['지역'].unique()
+# print(a)
+# print(b)
+# print(set(a)-set(b))
 
 #결측값
-#중복값
-#이상값
+train['지하철'].fillna(0,inplace=True)
+train['버스'].fillna(0,inplace=True)
+
+train[['임대보증금','임대료']] = train[['임대보증금', '임대료']].fillna("0").replace("-", "0").astype(int)
+
+print(train.isna().sum())
+
+#라벨인코딩
+from sklearn.preprocessing import LabelEncoder
+#단지코드는 나중에
+label_lst = ['임대건물구분', '지역', '공급유형','자격유형']
+
+for i in label_lst:
+    train[i] = LabelEncoder().fit_transform(train[i])
+
+print(train.info())
+#임대료에 - 를 0으로 바꿔줘야함
+#자격유형 B, F, O가 test에는 없음 - 삭제해도 될 듯 / test에 nan값 있음
+#지하철/정류장 수도 2개를 넘어가면 2, 1개면 1, 없으면 0 3가지로 해도 괜찮을 듯
+#공급유형 - test에는 '장기전세', '공공분양', '공공임대(5년)' 없음
+#test에 서울특별시 없음
+
 #상관계수
+# 상관계수 계산
+import seaborn as sns
+
+correlation_matrix = train.drop('단지코드',axis=1).corr()
+
+# 히트맵 그리기
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
+
+# 플롯 제목 추가
+plt.title('Correlation Heatmap')
+
+# 그래프 표시
+plt.show()
+
+# 컬럼별 상관계수 산점도 하나씩 보기
+# for i in (train.drop('단지코드',axis=1).columns):
+#
+#     #상관계수 산점도
+#     sns.scatterplot(x=i, y='등록차량수', data=train.drop('단지코드',axis=1))
+#
+#     # 플롯 제목 추가
+#     plt.title('Correlation Heatmap')
+#
+#     # 그래프 표시
+#     plt.show()
+
+cor_lst = abs(correlation_matrix['등록차량수']).sort_values(ascending=False).head(6).reset_index(name='상관계수')
+cor_lst = np.array(cor_lst['index'])[1:]
+print(cor_lst)
+#['단지내주차면수' '임대료' '공급유형' '임대건물구분' '임대보증금']
+
+#중복값
+#이상값 (unique)
+#병합할 컬럼 있는지
+
 
 #여러모델 평균값 앙상블 (xgb,randomforest,lgbm)
 #catboost
