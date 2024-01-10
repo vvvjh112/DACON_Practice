9159f767ce7bd605c09aa6efe2fecd57d695ee8c
lleell6 <lleell6@naver.com>
2024-01-10 20:33:54 +0900

    Changes

--- Daegu/First.py
+++ Daegu/First.py
@@ -16,8 +16,24 @@ test = pd.read_csv('test.csv')
 countrywide = pd.read_csv('external_open/countrywide_accident.csv') #대구 제외한 교통사고 정보
 submission = pd.read_csv('sample_submission.csv')
 
+#test셋에는 없는 값들 확인
+train_column = train.columns
+test_column = ['기상상태','시군구','도로형태','노면상태','사고유형']
+lst = {}
+for i in test_column:
+    set_train = set(train[i].unique())
+    set_test = set(test[i].unique())
+    lst[i] = set_train-set_test
+
+for i in lst.keys():
+    print(i,':',lst[i])
+
+#안개가 test엔 없는것 확인 - 삭제
+train = train[train['기상상태']!= '안개']
+
 #기상상태, 요일별, 월별 ECLO 시각화해보기
 train['사고일시'] = pd.to_datetime(train['사고일시'],format="%Y-%m-%d %H")
+test['사고일시'] = pd.to_datetime(test['사고일시'],format="%Y-%m-%d %H")
 train['연'] = train['사고일시'].dt.year
 train['월'] = train['사고일시'].dt.month
 train['일'] = train['사고일시'].dt.day
@@ -79,17 +95,105 @@ print(group_days.head(12))
 
 gm = group_month.plot(title='월별 평균',kind='line',marker='o')
 gm.set_xticks(group_month.index)
-plt.show()
+# plt.show()
 
 gd = group_day.plot(title='일별 평균',kind='line',marker='o')
 gd.set_xticks(group_day.index)
-plt.show()
+# plt.show()
 
 gh = group_hour.plot(title='시간별 평균',kind='line',marker='o')
 gh.set_xticks(group_hour.index)
-plt.show()
+# plt.show()
 
 gds = group_days.plot(title='요일별 평균',kind='line',marker='o',x='요일')
-plt.show()
+# plt.show()
 
 #공휴일 체크
+print(train['사고일시'].dt.year.unique())
+print(test['사고일시'].dt.year.unique())
+# train[2019 2020 2021], test[2022]
+#공휴일 api를 통해 가져오기(대체휴무일때문)
+import requests
+from datetime import datetime
+import json
+from pandas import json_normalize
+
+def getholiday(year):
+    today_year = year
+
+    KEY = "JWgg0HGk6X1%2FiSamZNl29O5awvu46mP%2BwM%2Fj8WNoLfNNfMeo2zhjPECwNdheapXHpIKbEZ0GCg1sWUm%2BrTdBfg%3D%3D"
+    url = (
+        "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo?_type=json&numOfRows=50&solYear="
+        + str(today_year)
+        + "&ServiceKey="
+        + str(KEY)
+    )
+    response = requests.get(url)
+    if response.status_code == 200:
+        json_ob = json.loads(response.text)
+        holidays_data = json_ob["response"]["body"]["items"]["item"]
+        dataframe = json_normalize(holidays_data)
+    # dateName = dataframe.loc[dataframe["locdate"] == int(today), "dateName"]
+    # print(dateName)
+    result = dataframe["locdate"].astype(str)
+    return result.to_list()
+
+holiday_2019 = getholiday(2019)
+holiday_2020 = getholiday(2020)
+holiday_2021 = getholiday(2021)
+holiday_2022 = getholiday(2022)
+print(holiday_2019)
+def check_holiday(x):
+    print(type(x))
+    year = x.dt.year
+    date = x.dt.strftime('%Y%m%d')
+    if year == 2019:
+        if date in holiday_2019:
+            return 1
+    elif year == 2020:
+        if date in holiday_2020:
+            return 1
+    elif year == 2021:
+        if date in holiday_2021:
+            return 1
+    elif year == 2022:
+        if date in holiday_2022:
+            return 1
+    return 0
+print(train.info())
+train['공휴일'] = train['사고일시'].apply(check_holiday)
+test['공휴일'] = test['사고일시'].apply(check_holiday)
+
+print(train.head(20))
+print(test.head(20))
+
+#평일이면 평일로구분 주말 or 공휴일이면 휴일로 구분하는것도 괜찮을듯 싶은데
+
+#상관계수 (라벨링 후 해야할 듯)
+# 상관계수 계산
+import seaborn as sns
+#
+# correlation_matrix = train.drop(['ECLO','연','월','일'],axis=1).corr()
+# #
+# # # 히트맵 그리기
+# sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
+# #
+# # # 플롯 제목 추가
+# plt.title('Correlation Heatmap')
+# #
+# # # 그래프 표시
+# plt.show()
+
+# 컬럼별 상관계수 산점도 하나씩 보기
+# for i in (train.drop('단지코드',axis=1).columns):
+#
+#     #상관계수 산점도
+#     sns.scatterplot(x=i, y='등록차량수', data=train.drop('단지코드',axis=1))
+#
+#     # 플롯 제목 추가
+#     plt.title('Correlation Heatmap')
+#
+#     # 그래프 표시
+#     plt.show()
+
+#단지 예측 뿐 아니라 train 셋에 있는 데이터를 바탕으로 사고를 줄일 수 있는 방법 제시.
